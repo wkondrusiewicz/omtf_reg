52c949805b8893a14d2a675f1d2a532aff261469
Wiktor <vikkon00@gmail.com>
2019-11-03 15:10:19 +0100

    clean up v4

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(
+    name='omtf_reg',
+    version='0.1',
+    description='Regresion tool for OMTF',
+    packages=find_packages(include=['omtf_reg']),
+    install_requires=[
+    ],
+)

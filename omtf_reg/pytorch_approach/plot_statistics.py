1fd05d7873f97c63497755bc99204484c3d20818
Wiktor <vikkon00@gmail.com>
2019-11-05 16:14:02 +0100

    further plots development

--- omtf_reg/pytorch_approach/plot_statistics.py
+++ omtf_reg/pytorch_approach/plot_statistics.py
@@ -5,7 +5,11 @@ import os
 import glob
 import argparse
 
+from tqdm import tqdm
+
 from sklearn.metrics import r2_score
+
+from omtf_reg.pytorch_approach.constants import pt_intervals
 
 
 def parse_args():
@@ -29,10 +33,9 @@ def extract_predictions_and_labels(experiment_dirpath: str, fn_to_apply):
 
     train_stats = []
     valid_stats = []
-    for i, (label_path, pred_path) in enumerate(zip(label_paths, prediction_paths)):
+    for i, (label_path, pred_path) in tqdm(enumerate(zip(label_paths, prediction_paths)), desc='Extracting data from npz files'):
         labels = np.load(label_path)['data']
         preds = np.load(pred_path)['data']
-        print(i)
         if 'TRAIN' in label_path:
             train_stats.append(fn_to_apply(labels, preds))
         else:
@@ -44,7 +47,7 @@ def get_pull(labels, preds):
     return np.mean((preds - labels) / labels)
 
 
-def draw_effectivness_curve(test_data: dict, pt_intervals: list, cut: int, figsize: tuple = (8, 4), outpath=None):
+def draw_effectivness_curve(test_data: dict, pt_intervals: list, cut: float, figsize: tuple = (8, 4), outpath=None):
     labels = test_data['labels']
     preds = test_data['predictions']
     h2 = np.histogram(labels[preds > cut], pt_intervals)[0]
@@ -89,7 +92,7 @@ def draw_r2_scores(experiment_dirpath: str, figsize: tuple = (8, 4), outpath=Non
     epochs = len(r2_train)
     plt.plot(range(epochs), r2_train, label='TRAIN', color='b')
     plt.plot(range(epochs), r2_valid, label='VALID', color='y')
-    plt.title(f'Loss for training for {epochs} epochs')
+    plt.title(f'$r^2$ score for training for {epochs} epochs')
     plt.xlabel('Epochs')
     plt.ylabel('r2_score')
     plt.legend()
@@ -108,7 +111,7 @@ def draw_pull(experiment_dirpath: str, figsize: tuple = (8, 4), outpath=None):
     epochs = len(pull_train)
     plt.plot(range(epochs), pull_train, label='TRAIN', color='b')
     plt.plot(range(epochs), pull_valid, label='VALID', color='y')
-    plt.title(f'Loss for training for {epochs} epochs')
+    plt.title(f'Pull for training for {epochs} epochs')
     plt.xlabel('Epochs')
     plt.ylabel('Pull')
     plt.legend()
@@ -122,7 +125,16 @@ def draw_pull(experiment_dirpath: str, figsize: tuple = (8, 4), outpath=None):
 
 def main():
     args = parse_args()
-
+    plots_path = os.path.join(args.experiment_dirpath, 'plots')
+    os.makedirs(plots_path, exist_ok=True)
+    with open(os.path.join(args.experiment_dirpath, 'losses.json'), 'r') as f:
+        json_data = json.load(f)
+    draw_losses(json_data, outpath=os.path.join(plots_path, 'losses.pdf'))
+    draw_r2_scores(args.experiment_dirpath, outpath=os.path.join(plots_path, 'r2_scores.pdf'))
+    draw_pull(args.experiment_dirpath, outpath=os.path.join(plots_path, 'pulls.pdf'))
+    test_data = np.load(os.path.join(args.experiment_dirpath, 'test', 'labels_and_preds.npz'), allow_pickle=True)['data'][()]
+    for cut in pt_intervals:
+        draw_effectivness_curve(test_data, pt_intervals, cut=cut, outpath=os.path.join(plots_path, f'effectivness_curve_cut_{cut}.pdf'))
 
 if __name__ == '__main__':
     main()
